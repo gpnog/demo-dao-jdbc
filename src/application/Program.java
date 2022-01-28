073d256237990a688d71a05a3d404e11f121aa50
Gabriel Pedro Nogueira <gabrielpedronogueira@gmail.com>
2022-01-28 17:03:03 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -26,6 +26,14 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		System.out.println();
+		
+		System.out.println("=== TESTE 3: seller findAll ===");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+		
 	}
 
 }
