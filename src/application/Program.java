451d124c6db0d3fd12a2c7d308856099c94c82e8
Gabriel Pedro Nogueira <gabrielpedronogueira@gmail.com>
2022-01-28 17:24:32 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,34 +13,43 @@ public class Program {
 	public static void main(String[] args) {
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
 		System.out.println("=== TESTE 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		
+
 		System.out.println();
-		
+
 		System.out.println("=== TESTE 2: seller findByDepartment ===");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+
 		System.out.println();
-		
+
 		System.out.println("=== TESTE 3: seller findAll ===");
 		list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+
 		System.out.println();
-		
+
 		System.out.println("=== TESTE 4: seller insert ===");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+
+		System.out.println();
+
+		System.out.println("=== TESTE 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+		
 		
 	}
 
